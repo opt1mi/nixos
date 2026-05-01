7fab8b11d73e691a531fbb5dc83bd9d19f196bcc
Opt1mi <optimi@destable.hu>
2026-05-01 13:15:10 +0200

    Add home configuration for user 'optimi'

---
+++ home.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "optimi";
+  home.homeDirectory = "/home/optimi";
+  programs.git.enable = true;
+  home.stateVersion = "25.11";
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      rebuild = "sudo nixos-rebuild switch --flake ~/.nixos-dots#optinix";
+    };
+  };
+
+  home.file.".config/alacritty".source = ./config/alacritty;
+  home.file.".config/i3".source = ./config/i3;
+
+  home.packages = with pkgs; [
+    neovim
+    ripgrep
+    nil
+    nixpkgs-fmt
+    nodejs
+    gcc
+    zed-editor
+    discord
+    yazi
+    unzip
+    zip
+    noriskclient-launcher
+    obs-studio
+    fastfetch
+    prismlauncher
+    freecad
+    jp2a
+    kdePackages.kdenlive
+  ];
+
+}
